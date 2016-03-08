ce61810c52f0a68c1c45b494e8d76d758a7cc783
Manpreet Singh <manpreet28111995@gmail.com>
2016-03-08 18:04:14 +0530

    Added Signin Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :mobile, :email_id, :house_no, :locality, :pincode)
+      params.require(:user).permit(:name, :mobile, :email_id, :house_no, :locality, :pincode,:email)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :stores
-  devise_for :users
+  
   resources :bookings
    resources :stores
   resources :users
   root to: "users#index"
+
+  devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' 
+  end
  
 end
